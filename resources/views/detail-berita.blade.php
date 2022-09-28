454d40660c2d19f59b6922dfb117aee576539666
rizki140699 <rizkimaulana140699@gmail.com>
2022-09-28 20:39:55 +0700

    change font style

--- resources/views/detail-berita.blade.php
+++ resources/views/detail-berita.blade.php
@@ -3,9 +3,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <span class="badge text-bg-info">{{ $berita->kategori->nama }}</span>
-            <h2 style="font-weight: 700;">{{ $berita->judul_berita }}</h2>
+            <h2 class="mt-2" style="font-weight: 700;">{{ $berita->judul_berita }}</h2>
             <img src="{{ $berita->foto }}" style="width: 100%;max-height: 400px;" class="mt-3"/>
-            <p style="line-height: 28px;" class="mt-5">{{ $berita->isi_berita }}</p>
+            <p style="line-height: 32px;" class="mt-5">{{ $berita->isi_berita }}</p>
         </div>
     </div>
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet">
+    <style>
+      body, html {
+        font-family: 'Merriweather', serif;
+      }
+    </style>
   </head>
   <body>
 
